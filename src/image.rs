9a082d315438c984eceff87e4ed1d6da26d6de15
James Butcher <jamesbutcher167@gmail.com>
2022-04-10 16:43:59 +0100

    Simplify process by multiplying the vec3 by 255

--- src/image.rs
+++ src/image.rs
@@ -15,10 +15,12 @@ impl Image {
     }
 
     pub fn create_with_colour(width: usize, height: usize, default_colour: f32) -> Self {
+        let default_colour_rgb = Vec3::new(default_colour, default_colour, default_colour);
+
         Image {
             width,
             height,
-            data: vec![default_colour; 3 * width * height],
+            data: vec![default_colour_rgb; width * height],
         }
     }
 
@@ -41,24 +43,27 @@ impl Image {
         file.write_all(header.as_bytes())
             .expect("Couldn't write header");
 
-        let to_be_written = self.data
+        let to_be_written = self
+            .data
             .iter()
-            .map(|e| ((e * 255.) as u8))
-            .collect::<Vec<_>>();
-
+            .copied()
+            .map(|e| e * 255.)
+            .flat_map(|e| {
+                [e.r(), e.g(), e.b()]
+                    .iter()
+                    .map(|x| *x as u8)
+                    .collect::<Vec<u8>>()
+            })
+            .collect::<Vec<u8>>();
         file.write_all(&to_be_written).expect("Couldn't write data");
     }
 
     pub fn save_as_bmp(&mut self, filepath: &str) {
         let mut file = File::create(filepath).expect("Couldn't open file");
-
-
     }
 
     pub fn set_pixel(&mut self, x: usize, y: usize, rgb: Vec3) {
         let idx = (x + y * self.width) * 3;
-        self.data[idx] = rgb.r();
-        self.data[idx + 1] = rgb.g();
-        self.data[idx + 2] = rgb.b();
+        self.data[idx] = rgb;
     }
 }
